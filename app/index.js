1b6a7574fc2dd092e3fe0a15586ef76d720fc593
Jonathan Moore <jonathan@stylehatch.co>
2013-07-01 22:46:25 -0700

    Add basic Yeoman generator structure

--- app/index.js
+++ app/index.js
@@ -1,1 +1,50 @@
 'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var TumblrGenerator = module.exports = function TumblrGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(TumblrGenerator, yeoman.generators.Base);
+
+TumblrGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+TumblrGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/templates');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+TumblrGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
